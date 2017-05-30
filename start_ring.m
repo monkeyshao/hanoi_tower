5d753b9da5803db1894ff77fa9600e854004c318
monkeyshao <monkeyshao10@163.com>
2017-05-30 08:22:13 +0800

    Update start_ring.m

--- start_ring.m
+++ start_ring.m
@@ -1,4 +1,6 @@
 function c=start_ring(n,k1)
+% part of work in painting the background
+% initial a cell to store the axis of the pan
 c=cell(1,n);
 c{n}=[1+(2*n+3)*(k1-1),2*n+1+(2*n+3)*(k1-1),2*n+1+(2*n+3)*(k1-1),1+(2*n+3)*(k1-1);0,0,1,1];
 for i=n-1:-1:1
@@ -6,4 +8,3 @@ for i=n-1:-1:1
     c{i}(1,:)=c{i}(1,:)+[1,-1,-1,1];
     c{i}(2,:)=c{i}(2,:)+1;
 end
-%initial a cell to store the axis of the pan
